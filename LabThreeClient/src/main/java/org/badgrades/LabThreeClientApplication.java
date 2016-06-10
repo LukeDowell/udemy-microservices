982de340bd04d0d875ce490432dfc5598d14374e
Luke Dowell <lukedowell@gmail.com>
2016-06-10 11:48:01 -0500

    Adds lab 4

---
+++ LabThreeClient/src/main/java/org/badgrades/LabThreeClientApplication.java
@@ -0,0 +1,12 @@
+package org.badgrades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabThreeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabThreeClientApplication.class, args);
+	}
+}
